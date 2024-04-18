a38ac4619d516f76d123dc41074de1c691b1da26
MeDear <110712482+MeDear0-0@users.noreply.github.com>
2024-04-18 13:44:20 +0700

    Add files via upload

---
+++ 201-re/HW-Mo2/Student_test-form/form.js
@@ -0,0 +1,21 @@
+//Write your student id, firstname, and lastname in a single line comment before starting your program
+//students who do not put this comment will get 50% taken off their score.
+//ID:       Name:
+ 
+function init() {
+ //insert your code here
+}
+
+function showForm() {
+//insert your code here
+}
+
+function display() {
+//insert your code here
+}
+
+function clear() {
+//insert your code here
+}
+
+init();
